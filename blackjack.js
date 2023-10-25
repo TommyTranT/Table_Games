f218057f252f3840d65cd608a4bc24ad2a14da3e
Tommy Tran <haitran1995@gmail.com>
2023-10-25 12:16:50 -0400

    Made code cleaner and concise

--- blackjack.js
+++ blackjack.js
@@ -1,3 +1,10 @@
-let amount = Math.ceil(Math.random() * 100) * 5;
+let bet = Math.ceil(Math.random() * 100) * 5;
 
-console.log(amount);
+blackJackPayout = () => {
+  let blackJack = bet / 2 + bet;
+  let response = "The Blackjack of " + bet + " is " + blackJack;
+
+  return response;
+};
+
+console.log(blackJackPayout());
